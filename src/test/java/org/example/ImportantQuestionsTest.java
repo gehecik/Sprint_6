d5d177ad94afc17b3171505d1922fb723c62b732
Kechek Karine <kechek.karine@gmail.com>
2026-03-28 18:42:41 +0300

    FAQ opening a drop-down list

--- src/test/java/org/example/ImportantQuestionsTest.java
+++ src/test/java/org/example/ImportantQuestionsTest.java
@@ -2,7 +2,15 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class ImportantQuestionsTest {
     @RegisterExtension
@@ -13,6 +21,21 @@ public class ImportantQuestionsTest {
         WebDriver driver = extension.getDriver();
 
         driver.get("https://qa-scooter.praktikum-services.ru/");
+
+        List<WebElement> elements = driver.findElements(By.className("accordion__button"));
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.elementToBeClickable(By.className("App_CookieButton__3cvqF")))
+                .click();
+
+        for (WebElement element : elements) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+
+            new WebDriverWait(driver, Duration.ofSeconds(5))
+                    .until(ExpectedConditions.elementToBeClickable(element)).click();
+        }
+
+        new WebDriverWait(driver, Duration.ofSeconds(5))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.className("accordion__button")));
     }
 
 }
