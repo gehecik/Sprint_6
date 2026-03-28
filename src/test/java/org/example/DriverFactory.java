25b72319ba2791968b59c4315a7f61ceb3a92f4c
Kechek Karine <kechek.karine@gmail.com>
2026-03-28 18:24:13 +0300

    add DriverExtension

--- src/test/java/org/example/DriverFactory.java
+++ src/test/java/org/example/DriverFactory.java
@@ -1,4 +1,33 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class DriverFactory {
+    private WebDriver driver;
+
+    public void startUp() {
+        if ("firefox".equals(System.getProperty("browser"))) {
+            startUpFirefox();
+        } else {
+            startUpChrome();
+        }
+    }
+
+    public void startUpChrome() {
+        WebDriverManager.chromiumdriver().browserVersion("146").setup();
+        //WebDriverManager.chromiumdriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    public void startUpFirefox() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 }
--- src/test/java/org/example/ImportantQuestionsTest.java
+++ src/test/java/org/example/ImportantQuestionsTest.java
@@ -1,38 +1,18 @@
 package org.example;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.List;
 
 public class ImportantQuestionsTest {
-    private final DriverFactory factory = new DriverFactory();
-
-    @BeforeEach
-    public void startUp() {
-        factory.startUp();
-    }
+    @RegisterExtension
+    private final DriverExtension extension = new DriverExtension();
 
     @Test
     public void openMainPage() throws Exception {
-        WebDriver driver = factory.getDriver();
+        WebDriver driver = extension.getDriver();
 
         driver.get("https://qa-scooter.praktikum-services.ru/");
     }
 
-    @AfterEach
-    public void tearDown() {
-        factory.getDriver().quit();
-    }
 }
