b13e80ebca2aaa054e0022e49e0546f34625b13e
Kechek Karine <kechek.karine@gmail.com>
2026-03-28 18:00:13 +0300

    add opening throught two browsers: firefox chrome

--- src/test/java/org/example/ImportantQuestionsTest.java
+++ src/test/java/org/example/ImportantQuestionsTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -20,9 +21,22 @@ public class ImportantQuestionsTest {
 
     @BeforeEach
     public void startUp() {
+        if ("firefox".equals(System.getProperty("browser"))) {
+            startUpFirefox();
+        } else {
+            startUpChrome();
+        }
+    }
+
+    public void startUpChrome() {
         WebDriverManager.chromiumdriver().browserVersion("146").setup();
         //WebDriverManager.chromiumdriver().setup();
         driver = new ChromeDriver();
+    }
+
+    public void startUpFirefox() {
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
     }
 
     @Test
